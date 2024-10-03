8b04ce10ac1ecee9f6e7bcda0fa2e0e5e9e6b523
henry753951 <henryboy753951@gmail.com>
2024-10-03 12:12:03 +0800

    Refactor SCSS preprocessor options in nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -31,7 +31,7 @@ export default defineNuxtConfig({
     css: {
       preprocessorOptions: {
         scss: {
-          api: "modern",
+          api: "modern-compiler",
         },
       },
     },
