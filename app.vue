eaf91fc40a018838d59c7053ff31145294484f6d
henry753951 <henryboy753951@gmail.com>
2024-10-07 04:07:41 +0800

    remove dark mode

--- app.vue
+++ app.vue
@@ -12,4 +12,12 @@
   </div>
 </template>
 
+<script lang="ts" setup>
+// add light class to body
+onMounted(() => {
+  document.body.classList.add("light");
+}); 
+
+</script>
+
 <style lang="scss"></style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,6 +9,9 @@ export default defineNuxtConfig({
     options: {
       theme: {
         preset: Noir,
+        options: {
+          darkModeSelector: ".dark",
+        },
       },
     },
   },
